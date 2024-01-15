742cbd4ba0bf063ddad58bbc67ce2eb487bc35d8
Chiranth Jain <chiranth.k@logically.co.uk>
2024-01-15 18:49:45 +0530

    added firestore

--- app.py
+++ app.py
@@ -7,10 +7,17 @@ from time import sleep
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from datetime import datetime
+
+cred = credentials.Certificate("/etc/secrets/creds.json")
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 app = FastAPI()
 
-last_movie = ""
 bot = telegram.Bot(os.environ["BOT_KEY"])
 
 
@@ -24,15 +31,10 @@ async def send_message(message):
         print(e)
 
 
-def get_last_movie():
-    global last_movie
-    with open('last_movie.txt', 'r') as file:
-        last_movie = file.read()
-
-
 @app.get("/track/")
 async def scrape_site():
-    get_last_movie()
+    last_movie = db.collection(
+        'latest-movie').document('LHU2CQpehHqVgXDaQNXX').get().to_dict()
     url = 'https://www.1tamilblasters.tel/'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -42,13 +44,15 @@ async def scrape_site():
         'li', class_='ipsWidget ipsWidget_horizontal ipsBox ipsResponsive_block')
     div = li.find("div", "ipsWidget_inner ipsPad ipsType_richText")
     p_tags = div.find_all("p")[3:]
-    with open('last_movie.txt', 'w') as file:
-        file.write(p_tags[0].get_text().strip().strip("-").strip())
+    updated_doc = {"title": p_tags[0].get_text().strip().strip(
+        "-").strip(), "timestamp": datetime.now().isoformat()}
+    db.collection(
+        'latest-movie').document('LHU2CQpehHqVgXDaQNXX').update(updated_doc)
     for index, item in enumerate(p_tags):
         for tag in item.find_all():
             tag.unwrap()
         p_tags[index] = item.get_text().strip().strip("-").strip()
-    filtered_movies= p_tags[:p_tags.index(last_movie)]
+    filtered_movies = p_tags[:p_tags.index(last_movie['title'])]
     for p in filtered_movies:
         await send_message(p)
     return JSONResponse(status_code=200, content=jsonable_encoder({"movies": filtered_movies}), media_type="application/json")
